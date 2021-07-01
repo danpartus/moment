e15c13eccebccbc1c63bbc93aa429441d1d464dc
dartus <dartus@singlestoneconsulting.com>
2021-07-01 09:05:19 -0400

    Updates to structure of UI

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { AddPostComponent } from './add-post/add-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostListComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
